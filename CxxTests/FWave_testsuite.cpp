34c65e17baec3eedadd9af3483f026ff95e94c7b
Alexander Binsmaier <abinsmaier@gmail.com>
2014-10-23 12:33:22 +0200

    added include for FWave.hpp

--- CxxTests/FWave_testsuite.cpp
+++ CxxTests/FWave_testsuite.cpp
@@ -19,16 +19,16 @@ int main( int argc, char *argv[] ) {
     return status;
 }
 bool suite_FWaveTest_init = false;
-#include "/home/ludwig/SWE1D/CxxTests/FWave_testsuite.t.h"
+#include "/home/sascha/Dokumente/Projects/TsunSim/swe1d/CxxTests/FWave_testsuite.t.h"
 
 static FWaveTest suite_FWaveTest;
 
 static CxxTest::List Tests_FWaveTest = { 0, 0 };
-CxxTest::StaticSuiteDescription suiteDescription_FWaveTest( "CxxTests/FWave_testsuite.t.h", 3, "FWaveTest", suite_FWaveTest, Tests_FWaveTest );
+CxxTest::StaticSuiteDescription suiteDescription_FWaveTest( "CxxTests/FWave_testsuite.t.h", 4, "FWaveTest", suite_FWaveTest, Tests_FWaveTest );
 
 static class TestDescription_suite_FWaveTest_test_output : public CxxTest::RealTestDescription {
 public:
- TestDescription_suite_FWaveTest_test_output() : CxxTest::RealTestDescription( Tests_FWaveTest, suiteDescription_FWaveTest, 6, "test_output" ) {}
+ TestDescription_suite_FWaveTest_test_output() : CxxTest::RealTestDescription( Tests_FWaveTest, suiteDescription_FWaveTest, 7, "test_output" ) {}
  void runTest() { suite_FWaveTest.test_output(); }
 } testDescription_suite_FWaveTest_test_output;
 
